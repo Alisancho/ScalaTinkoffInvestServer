1829713240236096f875fa3ad417e736fd6add5b
alisancho <sashabig4328@protonmail.com>
2020-05-10 00:29:35 +0300

    new

--- src/main/scala/ru/invest/AppStart.scala
+++ src/main/scala/ru/invest/AppStart.scala
@@ -5,11 +5,18 @@ import java.util.logging.Logger
 
 import akka.http.scaladsl.Http
 import cats.effect.ExitCode
-import ru.invest.core.config.ConfigObject.{SERVER_HOST, SERVER_PORT, TINKOFF_BROKER_ACCOUNT_ID, TOKEN}
+import ru.invest.core.config.ConfigObject.{
+  SERVER_HOST,
+  SERVER_PORT,
+  TINKOFF_BROKER_ACCOUNT_ID,
+  TOKEN,
+  SCHEDULER_POOL_DB,
+  SCHEDULER_POOL_TINKOFF,
+  POOL_OPENAPI
+}
 import ru.invest.service.{
   BusinessProcessServiceImpl,
   DataBaseServiceImpl,
-  MathServiceImpl,
   MonitoringServiceImpl,
   TinkoffRESTServiceImpl
 }
@@ -47,11 +54,11 @@ trait AppStartHelper {
   implicit val system: ActorSystem          = ActorSystem()
   implicit val ec: ExecutionContextExecutor = system.dispatcher
   implicit val ctx: MyContext               = new MyContext()
-  val schedulerDB: SchedulerService         = Scheduler.fixedPool(name = "my-fixed-db", poolSize = 4)
-  val schedulerTinkoff: SchedulerService    = Scheduler.fixedPool(name = "my-fixed-tinkoff", poolSize = 4)
+  val schedulerDB: SchedulerService         = Scheduler.fixedPool(name = "my-fixed-db", poolSize = SCHEDULER_POOL_DB)
+  val schedulerTinkoff: SchedulerService    = Scheduler.fixedPool(name = "my-fixed-tinkoff", poolSize = SCHEDULER_POOL_TINKOFF)
   val apiTask: Task[OpenApi] = for {
     log <- Task { Logger.getLogger("Pooo") }
-    api <- Task { new OkHttpOpenApiFactory(TOKEN, log).createOpenApiClient(Executors.newFixedThreadPool(4)) }
+    api <- Task { new OkHttpOpenApiFactory(TOKEN, log).createOpenApiClient(Executors.newFixedThreadPool(POOL_OPENAPI)) }
   } yield api
 
 }
