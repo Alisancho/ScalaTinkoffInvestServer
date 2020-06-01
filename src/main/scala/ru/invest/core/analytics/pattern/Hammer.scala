ff1d35bca9fa073a3a2bac7d8b098fe4b710df14
alisancho <sashabig4328@protonmail.com>
2020-06-01 20:21:21 +0300

    add analytics absorption

--- src/main/scala/ru/invest/core/analytics/pattern/Hammer.scala
+++ src/main/scala/ru/invest/core/analytics/pattern/Hammer.scala
@@ -6,6 +6,7 @@ import ru.invest.entity.database.AnalyticsTbl
 import ru.tinkoff.invest.openapi.models.market.{Candle, HistoricalCandles}
 import AnalyticsTbl._
 import ru.invest.core.analytics.СandleMod._
+
 trait Hammer {
 
   def hammer(l: HistoricalCandles)(f: AnalyticsTbl => Task[_])(schedulerDB: SchedulerService): Task[_] =
@@ -21,18 +22,10 @@ trait Hammer {
       _  = if (trendDown(q1, q2, q3, q4) && q2.isHammer) f(l.toAnalyticsTbl("HUMMER", "DOWN")).runAsyncAndForget(schedulerDB)
     } yield ()
 
-  private val trendUp: (Candle, Candle, Candle, Candle) => Boolean = (q1, q2, q3, q4) => {
-    if (q4.middleWidth > q3.middleWidth && q3.middleWidth > q2.middleWidth && q2.middleWidth < q1.middleWidth)
-      true
-    else
-      false
-  }
+  private val trendUp: (Candle, Candle, Candle, Candle) => Boolean = (q1, q2, q3, q4) =>
+    q4.middleWidth > q3.middleWidth && q3.middleWidth > q2.middleWidth && q2.middleWidth < q1.middleWidth
 
-  private val trendDown: (Candle, Candle, Candle, Candle) => Boolean = (q1, q2, q3, q4) => {
-    if (q4.middleWidth < q3.middleWidth && q3.middleWidth < q2.middleWidth && q2.middleWidth > q1.middleWidth)
-      true
-     else
-      false
-  }
+  private val trendDown: (Candle, Candle, Candle, Candle) => Boolean = (q1, q2, q3, q4) =>
+    q4.middleWidth < q3.middleWidth && q3.middleWidth < q2.middleWidth && q2.middleWidth > q1.middleWidth
 
 }
